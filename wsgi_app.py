291eee03ecd1fc5927dbcc1d89332b734dfb74f3
Francis Kang <fkang@mozilla.com>
2016-07-25 09:45:04 -0400

    Change from mod_wsgi/apache to Caddy/gunicorn
    Currently experiementing with using Caddy as the reverse proxy as it
    supports git deploy and https built in.

--- wsgi_app.py
+++ wsgi_app.py
@@ -1,11 +1,11 @@
 import sys,os
 sys.path.insert(0, '/app')
-sys.path.insert(0, '/app/roomplz')
+sys.path.insert(0, os.path.join(
+    os.path.dirname(__file__), "roomplz"
+))
 from exams import app as exrem
 from hello import app as roomplz
 from admin import app as adminapp
 from werkzeug.wsgi import DispatcherMiddleware
-
-os.chdir('/app/roomplz')
 
 application = DispatcherMiddleware(roomplz, {'/remind':exrem, '/admin':adminapp})
