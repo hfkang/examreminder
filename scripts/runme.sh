41fd945da904ae3f0ef38a1b13b3315de749a1df
Francis Kang <fkang@mozilla.com>
2016-08-28 22:22:27 -0400

    Update init script

--- scripts/runme.sh
+++ scripts/runme.sh
@@ -1,7 +1,10 @@
 #!/bin/sh
 echo "Startup"
+cd /app/roomplz
+echo "Init roomplz"
+python osm.py --all
+echo "Downloaded osm data"
 cd /app
-echo "Changed dir"
 nohup gunicorn --conf /app/gunicorn.conf wsgi_app:application > logs/gunicorn.log 2>&1 &
 echo "Started gunicorn"
 caddy
